d4d1a78fe9be9e3f4e62d7eff99651ae59a3b8b1
SergeyPanama <97026373+SergeyPanama@users.noreply.github.com>
2022-05-01 11:44:39 +0300

    Commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,37 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+     @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain1() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(3000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemain3() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
 }
